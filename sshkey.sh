e0bc359923cef97f3e9f1b247f355ffb4ae474aa
Root <24883953+Volarken@users.noreply.github.com>
2021-07-27 15:34:11 -0700

    Create sshkey.sh

---
+++ sshkey.sh
@@ -0,0 +1,116 @@
+#bin/bash
+#############################
+##Built by LukeSwetnam.com###
+#############################
+func_logEvent () {
+TIME0=$(date)
+LOGFILE="/var/sshkeylog.txt"
+sudo /bin/cat <<-EOM >>$LOGFILE
+        $LogInput $TIME0
+			EOM
+}
+
+
+function_enableKeys() {
+rm /home/user/.ssh/authorized_keys
+for file in ls /home/user/.ssh/keys/*
+do
+  ssh-keygen -i -f "$file" >> /home/user/.ssh/authorized_keys
+  done
+  LogInput="Authorization file updated..."
+  func_logEvent
+  echo $LogInput
+  echo "Press enter to exit..."
+  read -p ""
+  exit
+}
+
+function_removeKey() {
+echo "Here is a list of our current keys... "
+ls /home/user/.ssh/keys
+sleep 2;
+echo "Which key would you like to remove?"
+echo "If none, press enter"
+read -p '' -e REMOVE
+rm /home/user/.ssh/keys/$REMOVE
+LogInput="SSH Key $REMOVE has been deleted!"
+func_logEvent
+echo $LogInput
+sleep 2;
+echo "Would you like to remove another key?"
+echo "Y/N"
+read -p '' -e qValue
+echo ""
+if [[ "$qValue" == "Y" || "$qValue" == "y" ]]; then
+function_removeKey
+fi
+clear
+function_enableKeys
+}
+
+function_addKey() {
+echo "Please copy your key to the clipboard and prepare to paste into nano..."
+echo "What would you like to name this key?"
+read -p "" -e NAME
+nano /home/user/.ssh/keys/$NAME
+if test -f /home/user/.ssh/keys/$NAME
+then
+LogInput="SSH Key $NAME has been successfully added!"
+else
+LogInput="ERROR SSH Key $NAME has NOT added!"
+fi
+func_logEvent
+echo $LogInput
+echo "Would you like to add another key?"
+echo "Y/N"
+read -p '' -e qValue
+echo ""
+if [[ "$qValue" == "Y" || "$qValue" == "y" ]]; then
+function_addKey
+fi
+function_enableKeys
+}
+
+func_mainRun() {
+echo "$(tput setaf 2)"
+echo -e "
+Welcome to the Linux SSH Key Management System\n\n\
+1)Enable all Keys\n\
+2)Remove a Key\n\
+3)Upload a Key\n\
+4)Exit\n\
+"
+read -p '' -e MenuProcessor
+echo "$(tput sgr0)"
+if [[ "$MenuProcessor" = '1' ]]; then
+function_enableKeys
+fi
+if [[ "$MenuProcessor" = '2' ]]; then 
+function_removeKey
+fi
+if [[ "$MenuProcessor" = '3' ]]; then
+function_addKey
+fi
+if [[ "$MenuProcessor" = '4' ]]; then
+func_logEvent
+LogInput="Exit Function Executed at "
+exit
+fi
+}
+
+func_mainMenu() {
+echo "Welcome to Authorized Key Management Script, HOW TO USE:"
+echo "All keys inside of .ssh/keys will be authorized when you run this script, as well any keys removed from .ssh/keys will be deauthorized."
+echo "Would you like to run this now?"
+echo "Y/N"
+read -p '' -e qValue
+echo ""
+if [[ "$qValue" == "Y" || "$qValue" == "y" ]]; then
+func_mainRun
+else
+echo "Goodbye"
+exit
+fi
+}
+
+func_mainMenu
